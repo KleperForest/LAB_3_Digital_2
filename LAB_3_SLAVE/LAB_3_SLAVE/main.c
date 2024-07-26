eca4622ea466c88a8c7b2941509f62d5c2d15828
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-07-26 10:20:20 -0600

    s

--- LAB_3_SLAVE/LAB_3_SLAVE/main.c
+++ LAB_3_SLAVE/LAB_3_SLAVE/main.c
@@ -1,63 +1,114 @@
-/////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 // Universidad del Valle de Guatemala
 // IE3054: ELECTRÓNICA DIGITAL 2
 // Autor: Alan Gomez
-// Proyecto: LAB_3_SLAVE.c
-// Descripción: Laboratorio, comunicación SPI_Mode_Master.
+// Proyecto: LAB_2.c
+// Descripción: Laboratorio, librerias LCD, ADC y UART.
 // Hardware: ATmega328p
-// Created: 7/25/2024 10:56:31 AM
+// Created: 7/19/24 8:07 a.m.
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 #define F_CPU 16000000UL
+#define BAUD 9600
+#define MY_UBRR F_CPU/16/BAUD-1
 
 #include <avr/io.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <stdint.h>
 #include <util/delay.h>
 #include "LIB_me/ADC/ADC.h"
-#include "LIB_me/SPI/SPI.h"
+#include "LIB_me/UART/UART.h"
 
-uint8_t valorSPI = 0;
-uint8_t adc_value_1 = 0;
-uint8_t adc_value_2 = 0;
+////////////////////////////////////////////////////
+// Variables.
+////////////////////////////////////////////////////
+char buffer[64];  // Buffer para las cadenas de caracteres a mostrar en el LCD y UART
 uint16_t adc_results[2];  // Array para almacenar los resultados del ADC
+int counter = 0;  // Contador inicial
 
-void refreshPORT(uint8_t valor);
+int voltage1, voltage2; //Mapeo de ADC
+
+////////////////////////////////////////////////////
+// Prototipos de funciones
+////////////////////////////////////////////////////
+void display_menu(void);
+void process_command(char command);
+
+////////////////////////////////////////////////////
+// Programa Principal
+////////////////////////////////////////////////////
 
 int main(void)
 {
-	// Configurar Pines como salida
-	DDRD |= (1<<DDD2)|(1<<DDD3)|(1<<DDD4)|(1<<DDD5)|(1<<DDD6)|(1<<DDD7);
-	DDRB |= (1<<DDB0)|(1<<DDB1);
-	
-	PORTD &= ~((1<<DDD2)|(1<<DDD3)|(1<<DDD4)|(1<<DDD5)|(1<<DDD6)|(1<<DDD7));
-	PORTB &= ~((1<<DDB0)|(1<<DDB1));
-	
-	SPI_init(SPI_SLAVE_SS,SPI_Data_Order_MSB,SPI_Clock_IDLE_LOW,SPI_clock_First_EDGE);
-	ADC_Init();
+	// Configuración de puertos y periféricos
+	DDRD = 0xFF;  // Configura todos los pines del puerto D como salidas
+	DDRB = 0x0F;  // Configura los primeros dos pines del puerto B como salidas
+	ADC_Init();  // Inicializa el ADC
+	UART_Init(BAUD);  // Inicializa la UART
+
 	uint8_t adc_channels[] = {7, 6};  // Canales ADC a leer (ADC7 y ADC6)
-	SPCR |= (1<<SPIE); // Activar ISR SPI
-	sei();
-	
+
+
+	// Mostrar el menú inicial en la consola
+	display_menu();
+
 	while (1)
 	{
+		// Leer los valores de los ADC
 		ADC_Read_Multiple(adc_channels, adc_results, 2);
-		adc_value_1 = adc_results[0];
-		adc_value_2 = adc_results[1];
 		
+		// Convertir los valores ADC a voltaje
+		voltage1 = (int)(((adc_results[0] * 5.0) / 1023)*100);
+		voltage2 = (int)(((adc_results[1] * 5.0) / 1023)*100);
+
+
+		// Leer y procesar los comandos de la UART
+		if (UCSR0A & (1 << RXC0)) {  // Verificar si hay datos disponibles en el buffer de recepción
+			char received_char = UART_Receive();
+			process_command(received_char);
+		}
+
+		_delay_ms(500);  // Espera medio segundo antes de la siguiente actualización
 	}
 }
 
-ISR(SPI_STC_vect) {
-	valorSPI = SPDR;
-	if (valorSPI == 'c') {
-		SPI_send(adc_value_1);
-		} else if (valorSPI == 'd') {
-		SPI_send(adc_value_2);
+////////////////////////////////////////////////////
+// Funciones auxiliares
+////////////////////////////////////////////////////
+
+void display_menu(void) {
+	UART_TransmitString("\r\n***** Menu *****\r\n");
+	UART_TransmitString("1. Mostrar valores de ADC\r\n");
+	UART_TransmitString("2. Incrementar contador (+)\r\n");
+	UART_TransmitString("3. Decrementar contador (-)\r\n");
+	UART_TransmitString("4. Mostrar valor del contador\r\n");
+	UART_TransmitString("*****************\r\n");
+	UART_TransmitString("Seleccione una opción: ");
+}
+
+void process_command(char command) {
+	switch (command) {
+		case '1':
+		snprintf(buffer, sizeof(buffer), "Valores ADC - ADC6: %u, ADC7: %u\r\n", adc_results[1], adc_results[0]);
+		UART_TransmitString(buffer);
+		break;
+		case '+':
+		counter++;
+		UART_TransmitString("Contador incrementado.\r\n");
+		break;
+		case '-':
+		counter--;
+		UART_TransmitString("Contador decrementado.\r\n");
+		break;
+		case '4':
+		snprintf(buffer, sizeof(buffer), "Valor del contador: %d\r\n", counter);
+		UART_TransmitString(buffer);
+		break;
+		default:
+		UART_TransmitString("Opción inválida. Por favor, seleccione una opción válida.\r\n");
+		break;
 	}
+	display_menu();
 }
